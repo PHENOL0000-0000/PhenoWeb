11ef971612c1f0432547e72e38c18366a2e5e865
PHENOL0000-0000 <PHENOL0000.0000@gmail.com>
2024-07-08 09:21:46 +0000

    temp

--- phenosh.sh
+++ phenosh.sh
@@ -1,28 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 function ph_nlinf() {
-  echo '' >> $1
+  echo '' >> "$1"
 }
 
 function ph_makebashfile() {
-  touch $1.sh
-  echo '#/bin/sh' >> $1.sh
-  echo '' >> $1.sh
+  touch "$1.sh"
+  echo '#!/bin/bash' >> "$1.sh"
+  echo '' >> "$1.sh"
 }
 
 function ph_awrt() {
-  echo '${2}' >> $1
+  echo "$2" >> "$1"
 }
 
 function ph_aliastr() {
-  return $(('alias ${1}=\'${2}\''))
+  echo "alias $1='$2'"
 }
 
 ph_makebashfile phenoclone
 ph_makebashfile phenocloner
 ph_makebashfile phenon
-ph_awrt phenoclone.sh $(ph_aliastr phenoclone 'sh phenocloner.sh')
+ph_awrt phenoclone.sh "$(ph_aliastr phenoclone 'bash phenocloner.sh')"
 ph_awrt phenocloner.sh 'git clone https://github.com/$1/$2.git'
 ph_awrt phenon.sh 'source phenoclone.sh'
 ph_nlinf phenon.sh
-ph_awrt phnon.sh $(ph_aliastr phenon 'sh phenoclone.sh Phenol0000-0000')
+ph_awrt phenon.sh "$(ph_aliastr phenon 'bash phenoclone.sh Phenol0000-0000')"
